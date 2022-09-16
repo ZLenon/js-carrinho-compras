5c638e38acf2385129060a9ac095c6f3640800c5
lenon nascimento <lenoninformatica@gmail.com>
2022-09-15 23:20:33 -0300

    Algumas coisas feitas

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,6 +1,13 @@
-const fetchProducts = () => {
-  // seu código aqui
-};
+async function fetchProducts(parametro) {
+  try {
+    const url = `https://api.mercadolibre.com/sites/MLB/search?q=${parametro}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw new Error('You must provide an url');
+  }
+}
 
 if (typeof module !== 'undefined') {
   module.exports = {
