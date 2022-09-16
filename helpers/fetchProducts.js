8339fc2a2816b392903489394e34d77e47a15668
lenon nascimento <lenoninformatica@gmail.com>
2022-09-16 18:19:11 -0300

    indo aos poucos

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,12 +1,12 @@
-async function fetchProducts(parametro) {
+const fetchProducts = async (parameter) => {
   try {
-    const url = `https://api.mercadolibre.com/sites/MLB/search?q=${parametro}`;
+    const url = `https://api.mercadolibre.com/sites/MLB/search?q=${parameter}`;
     const response = await fetch(url);
     return await response.json();    
   } catch (error) {
     return error.message;
   }
-}
+};
 
 if (typeof module !== 'undefined') {
   module.exports = {
