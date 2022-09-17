2b11c100dc4302b66ef6c668bbc05a923d426620
lenon nascimento <lenoninformatica@gmail.com>
2022-09-17 20:25:50 -0300

    Ta saindo aos poucos esse trem🚝

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -1,9 +1,18 @@
 const localStorageSimulator = require('../mocks/localStorageSimulator');
 const saveCartItems = require('../helpers/saveCartItems');
 
+
 localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Testa se, ao executar saveCartItems com um cartItem como param, o método localStorage.setItem é chamado', () => {
+    expect.assertions(1);
+    saveCartItems('cartItem');
+    expect(localStorage.setItem).toHaveBeenCalled();
+  });
+  it('Testa se, ao executar saveCartItems com um cartItem como param, o método localStorage.setItem é chamado com dois parâmetros, sendo o primeiro a chave \'cartItems\' e o segundo sendo o valor passado como argumento para saveCartItems', () => {
+    expect.assertions(1);
+    saveCartItems('cartItem');
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', 'cartItem');
+  });
 });
